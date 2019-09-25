a0ef5f77f73efb4323ecf35b4eab5136b623de75
Georgii <ivannikovgeorge1337@gmail.com>
2019-09-25 15:43:23 +0200

    first commit. Added the canvas for the wesite. Entrails are in progress

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=2)])
+    confirm_password = PasswordField('Password confirmation',
+                                     validators=[EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=2)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
